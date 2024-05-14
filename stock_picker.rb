52a7bcc3ff9288809c7ab960bdde72aaf8e19337
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 15:38:53 -0700

    Add stock_picker.rb and pseduocode

---
+++ stock_picker.rb
@@ -0,0 +1,10 @@
+def stock_picker(stock_prices)
+    #have variables for best day to buy and sell 
+    #iterate through array
+    #have one day be current best day to sell
+    #go through the days to see what current best day 
+    ##to buy is and check for difference, save difference 
+    #if difference is higher for next pair save current bests
+    #keep going through array
+    #return array of the best day to buy and sell   
+end
