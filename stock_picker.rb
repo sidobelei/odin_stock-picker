30145b2a49a112a730d51c5489c47013b2ad3cc3
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-15 11:20:28 -0700

    Add stock_picker function
    The stock_picker function takes an array of prices
    and determines the best day (index) to buy and
    sell stocks.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,10 +1,22 @@
 def stock_picker(stock_prices)
-    #have variables for best day to buy and sell 
-    #iterate through array
-    #have one day be current best day to sell
-    #go through the days to see what current best day 
-    ##to buy is and check for difference, save difference 
-    #if difference is higher for next pair save current bests
-    #keep going through array
-    #return array of the best day to buy and sell   
+    best_sell_day_index = 0
+    best_buy_day_index = 0
+    profit = 0
+    offset = 0
+    stock_prices.each_with_index do |buy_price, index|
+        offset += 1
+        if offset < stock_prices.length
+            stock_prices.drop(offset).each_with_index do |sell_price, offset_index|
+                if sell_price - buy_price > profit
+                    profit = sell_price - buy_price
+                    best_sell_day_index = offset_index + offset
+                    best_buy_day_index = index
+                end
+            end
+        end 
+    end
+    p [best_buy_day_index, best_sell_day_index]
 end
+
+# Uncomment to run and change parameters
+#stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
